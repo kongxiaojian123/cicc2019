b1466cbc98d0696655bd3293183c95d1911a6902
djkong <djkong@corp.netease.com>
2019-01-31 14:44:16 +0800

    music

--- src/index/js/App.vue
+++ src/index/js/App.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="app">
+    <div class="app" ref="app">
         <transition name="fadeout">
             <Game v-if="pageIndex===0"/>
             <Loading v-if="pageIndex===-1" :assets="loadAssets" @complete="loadComplete"/>
         </transition>
+        <audio src="../assets/song.mp3" loop="loop" ref="audio"></audio>
     </div>
 </template>
 
@@ -17,6 +18,12 @@
                 loadAssets:require.context("../assets", true, /\.(png|jpg)$/i),
                 pageIndex:-1
             }
+        },
+        mounted(){
+            this.$refs.app.ontouchstart = ()=>{
+                this.$refs.app.ontouchstart = null;
+                this.$refs.audio.play();
+            };
         },
         methods:{
             loadComplete(res){
